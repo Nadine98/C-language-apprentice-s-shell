dba6edc7edeb741ba2215e4a46c961ec8719f6e1
Nadine <Nadine.Kais@ruhr-uni-bochum.de>
2021-11-30 23:35:22 +0100

    Code

--- clash.c
+++ clash.c
@@ -2,26 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>
 #include <error.h>
 
 #define DirLength  200
 
 static void error(char*);
-static void showPrompt();
+static void showPrompt(int i);
 
 int main(int argc, char*argv[]){
-	
+
 
 }
 
 
 static void error(char *msg){
 	perror(msg);
-	exit(EXIT_FAILURE);	
+	exit(EXIT_FAILURE);
 }
 
-static void showPrompt(){
-	
-	
+static void showPrompt(int i){
+
+    char *dir= (char *) malloc(DirLength*i*sizeof(dir));
+    if(dir==NULL)
+        error("malloc");
+
+    char *tmp=getcwd(dir,DirLength-1);
+
+    if (tmp==NULL && errno == ERANGE){
+        showPrompt(i++);
+    }else if(tmp== NULL && errno!=ERANGE)
+        error("getcwd");
+    else{
+        printf("%s: ", dir);
+        fflush(stdout);
+    }
+
+
 }
